71674dd62d47ce5c86101790679329f5064a5da1
unknown <andreispdev@gmail.com>
2026-06-25 11:50:29 +0800

    supabaseUrl

--- src/supabase.js
+++ src/supabase.js
@@ -3,4 +3,12 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl) {
+  throw new Error("VITE_SUPABASE_URL is missing");
+}
+
+if (!supabaseAnonKey) {
+  throw new Error("VITE_SUPABASE_ANON_KEY is missing");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
